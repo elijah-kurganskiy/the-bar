2cc7479bb5e0776ff1b5289ba8055b95b0b81e0b
Ilia Kurganskiy <ilya.kurganskii@gmail.com>
2021-02-23 23:05:20 +0400

    fix: prevent scroll background

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import "./App.scss";
 import { CocktailDetailPageComponent } from "./app/features/cocktail-detail/pages/cocktail-detail-page/cocktail-detail-page";
 import { CocktailListPageComponent } from "./app/features/cocktail-list/pages/cocktail-list-page/cocktail-list-page";
 import { BottomModalRoute } from "./app/features/common/components/bottom-modal-route/bottom-modal-route";
--- src/app/features/cocktail-list/pages/cocktail-list-page/cocktail-list-page.tsx
+++ src/app/features/cocktail-list/pages/cocktail-list-page/cocktail-list-page.tsx
@@ -1,5 +1,6 @@
+import cn from "classnames";
 import React, { useCallback } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import { cocktails } from "../../../../store/cocktails";
 import { HeaderComponent } from "../../../common/components/header/header";
 import { CocktailListComponent } from "../../components/cocktail-list/cocktail-list";
@@ -7,13 +8,18 @@ import "./cocktail-list-page.scss";
 
 function CocktailListPage() {
   const history = useHistory();
-
+  const location = useLocation();
+  const modalIsOpen = /cocktails\/\d/.test(location.pathname);
   const onSelectCocktail = useCallback((id) => {
     history.push(`/cocktails/${id}`);
   }, []);
 
   return (
-    <div className="cocktail-list-page">
+    <div
+      className={cn("cocktail-list-page", {
+        "cocktail-list-page--block-scroll": modalIsOpen,
+      })}
+    >
       <HeaderComponent className="cocktail-list-page_header" />
       <main className="main">
         <CocktailListComponent
